7244e438e1b94924cfa56ec56f74a5e4eb631492
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-12-24 07:03:50 +0100

    Enable option 'offset: 0' (would be replaced by the value of the path's stroke-width)

--- leaflet.textpath.js
+++ leaflet.textpath.js
@@ -206,7 +206,7 @@ var PolylineTextPath = {
         var textNode = L.SVG.create('text'),
             textPath = L.SVG.create('textPath');
 
-        var dy = options.offset || this._path.getAttribute('stroke-width');
+        var dy = 'offset' in options ? options.offset : this._path.getAttribute('stroke-width');
 
         textPath.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", '#'+id);
         textNode.setAttribute('dy', dy);
