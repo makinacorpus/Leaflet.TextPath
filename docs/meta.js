15cb24ec461008295d1af1c8afb1cb2dd88fcef5
Tom Pasero <tpa@makina-corpus.com>
2023-08-03 14:30:42 +0200

    Update Readme and demo

---
+++ docs/meta.js
@@ -0,0 +1,23 @@
+const logJson = async () => {
+    const reponse = await fetch('meta.json');
+    return await reponse.json();
+}
+
+(async () => {
+    const meta = await logJson()
+    document.querySelector("title").innerText= meta.name
+    document.querySelector(".demo-name").innerText= meta.name
+    if (meta.npm){
+        document.querySelector(".npm").href="https://www.npmjs.com/package/" + meta.npm
+        document.querySelector(".npm .title").innerText= meta.npm
+    }else{
+        document.querySelector(".npm").remove()
+    }
+    document.querySelector(".sources").href= meta.github
+    document.querySelector(".readme-md").src= meta.github.replace("github.com","raw.githubusercontent.com") + "HEAD/README.md"
+    if (meta.moreInfo){
+        document.querySelector(".moreInfo").innerText= meta.moreInfo
+    }
+})()
+
+
